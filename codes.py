271ca15bc8e9d1731fd6ec6bd26ad3e44cd05850
unnatigupta2320 <75303352+unnatigupta2320@users.noreply.github.com>
2021-06-21 16:37:42 +0530

    Update codes.py

--- codes.py
+++ codes.py
@@ -3,6 +3,16 @@ import matplotlib.pyplot as plt
 
 O = np.array(([0,0]))
 
+#Generate line points
+def line_gen(A,B):
+  len =10
+  x_AB = np.zeros((2,len))
+  lam_1 = np.linspace(0,1,len)
+  for i in range(len):
+    temp1 = A + lam_1[i]*(B-A)
+    x_AB[:,i]= temp1.T
+  return x_AB
+
 def ellipse_gen(a,b):
 	len = 50
 	theta = np.linspace(0,2*np.pi,len)
@@ -29,16 +39,16 @@ plt.plot(x[0,:],x[1,:],'r' ,label='ellipse')
 
 #Labelling points
 plt.plot(-5,0, 'o')
-plt.text(-5.4 ,-0.4,'F1')
+plt.text(-5.4 ,-0.4,'F1',weight='bold')
 plt.plot(5,0, 'o')
-plt.text(4.8 ,-0.4,'F2')
+plt.text(4.8 ,-0.4,'F2',weight='bold')
 plt.plot(0,0, 'o')
-plt.text(-0.7 ,-0.3,'O')
+plt.text(-0.7 ,-0.3,'O',weight='bold')
 
 #Labelling points
-plt.plot(6.40,0,'o')
+plt.plot(6.40,0,'o',color='g')
 plt.text(6.0 ,0,'A')
-plt.plot(0,4, 'o')
+plt.plot(0,4, 'o',color='g')
 plt.text(0,3.5,'B')
 
 #Plotting line OF1
